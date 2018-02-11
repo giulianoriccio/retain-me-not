8b45288a8d4e2eb14ea239f3c15f340d33a78756
Giuliano Riccio <cosmo87rg@gmail.com>
2018-02-11 03:54:21 +0100

    added seasonal items

--- src/background.js
+++ src/background.js
@@ -1,6 +1,7 @@
 chrome.storage.local.get(["storable_ids", "stackable_items"], data => {
     var storable_ids    = ("storable_ids"    in data) ? data.storable_ids   : [],
         salvageable_ids = ("salvageable_ids" in data) ? data.salvageable_ids: [],
+        seasonal_ids    = ("seasonal_ids"    in data) ? data.seasonal_ids   : [],
         stackable_items = ("stackable_items" in data) ? data.stackable_items: {},
         items           = {};
 
@@ -40,11 +41,20 @@ chrome.storage.local.get(["storable_ids", "stackable_items"], data => {
         chrome.storage.local.set({"salvageable_ids": salvageable_ids});
     });
 
+    $.get("https://eu.finalfantasyxiv.com/lodestone/playguide/db/shop/0ba5004ab8e/", response => {
+        (new DOMParser()).parseFromString(response, "text/html").querySelectorAll("#sys_shop_type_gil a.db_popup").forEach(item => {
+            seasonal_ids.push(item.href.match(/db\/item\/([a-z0-9]+)\//)[1]);
+        });
+
+        chrome.storage.local.set({"seasonal_ids": seasonal_ids});
+    });
+
     chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         if (storable_ids.length && salvageable_ids.length && Object.keys(stackable_items).length) {
             sendResponse({
                 "storable_ids"   : storable_ids,
                 "salvageable_ids": salvageable_ids,
+                "seasonal_ids"   : seasonal_ids,
                 "stackable_items": stackable_items
             });
         }
